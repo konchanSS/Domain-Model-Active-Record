a6aa0b04de83b40f3c4d38d9ef94c2f550ab54ff
konchanSS <b1447864@planet.kanazawa-it.ac.jp>
2020-02-04 15:37:01 +0900

    add controller

---
+++ app/helpers/revenue_recognitions_helper.rb
@@ -0,0 +1,2 @@
+module RevenueRecognitionsHelper
+end
---
+++ test/controllers/revenue_recognitions_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RevenueRecognitionsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
