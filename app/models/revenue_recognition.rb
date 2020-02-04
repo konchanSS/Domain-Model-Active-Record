857de212b7d29a30d56b20590fc77359ff18b5f5
konchanSS <b1447864@planet.kanazawa-it.ac.jp>
2020-02-04 16:13:15 +0900

    add initialize revenue recognition

--- app/models/revenue_recognition.rb
+++ app/models/revenue_recognition.rb
@@ -1,2 +1,14 @@
 class RevenueRecognition < ApplicationRecord
+  def initialize(amount, date)
+    self.amount = amount
+    self.recognized_on = date
+  end
+
+  def getAmount
+    self.amount
+  end
+
+  def isRecognizableBy(asOf)
+    asOf > self.recognized_on || asOf == self.recognized_on
+  end
 end
