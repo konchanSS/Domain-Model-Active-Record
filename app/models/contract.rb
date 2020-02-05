ae70e241964cb48460126bde4cb3086daa823051
konchanSS <b1447864@planet.kanazawa-it.ac.jp>
2020-02-05 15:56:46 +0900

    add model initializer

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,2 +1,9 @@
 class Contract < ApplicationRecord
+  has_one :revenue_recognition, dependent: destroy
+  has_one :product, dependent: destroy
+  def initialize(product, revenue, whenSigned)
+      self.product = product
+      self.revenue = revenue
+      self.signed_on = whenSigned
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,27 @@
 class Product < ApplicationRecord
+  def initialize(name, type)
+    self.name = name
+    self.type = type
+  end
+end
+
+class WordProcessor < Product
+  def initialize(name)
+    self.name = name
+    self.type = "W"
+  end
+end
+
+class Spreadsheet < Product
+  def initialize(name)
+    self.name = name
+    self.type = "S"
+  end
+end
+
+class Database < Product
+  def initialize(name)
+    self.name = name
+    self.type = "D"
+  end
 end
